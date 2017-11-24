ccf2661f631d75d269735b7accb19e7369a688cb
bjes <bjorn.estlind@dewire.com>
2017-11-24 18:17:26 +0100

    added inner area

---
+++ Assets/Scripts/InnerArea.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InnerArea : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.localScale = new Vector3 (gameObject.transform.localScale.x * 0.9999F, gameObject.transform.localScale.y, gameObject.transform.localScale.z * 0.9999F);
+	}
+}
